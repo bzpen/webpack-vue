fb9659f5a05d07f2e177335b6e5f748df5b0197a
kaito kuroba <3118659412@qq.com>
2022-05-23 17:45:28 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //打包时引入index.html
+const { VueLoaderPlugin } = require('vue-loader');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); //清除dist文件夹
+
+
+
+module.exports={
+  mode: 'development', // none,不压缩  production,压缩 , development,开发环境
+
+  entry: path.resolve(__dirname,'./src/main.js'), // 入口文件
+
+  output: {
+    path: path.resolve(__dirname, 'dist'), // 输出文件的路径
+
+    filename: 'js/[name].js' // 输出文件的名称
+  },
+  module: {
+    rules: [
+      {
+        // 加载/提取js文件
+        // vue-loader@next 
+        test: /\.vue$/,
+        use: [
+          'vue-loader',
+        ]
+      },
+      {
+        // 加载/提取css文件  
+        // style-loader
+        // css-loader
+        test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/, // 不编译node_modules下的文件
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './index.html'), // 指定模板文件
+      filename: 'index.html', // 指定输出文件名
+      title: '手搭vue开发环境' // 指定模板文件中的变量
+    }),
+    new VueLoaderPlugin(),
+    new CleanWebpackPlugin()
+  ],
+  devServer: {
+    static: './dist',
+    port: 8080,
+  }
+  // devServer: {
+  //   static: './dist', // 告诉服务器从哪里提供内容
+  //   port: 3000, // 指定端口
+  //   open: true, // 自动打开浏览器
+  //   hot: true, // 开启热更新
+  //   publicPath:"/" // 在开发环境中，配置这个参数，可以让服务器访问静态资源的路径
+  // } 
+}
